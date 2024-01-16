b882e21ba645fe27402799a0c3840b51563eab3b
ribeirocleiton77 <cri@sopasta.com.br>
2024-01-16 13:20:32 -0300

    incluindo depêndencias do projeto

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 const app = express();
 const PORT = 3000;
 
 // Conectar ao banco de dados MongoDB
-mongoose.connect('mongodb://localhost:27017/controle_financeiro', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on('error', console.error.bind(console, 'Erro de conexão ao MongoDB:'));
-mongoose.connection.once('open', () => {
-  console.log('Conectado ao MongoDB');
+mongoose.connect("mongodb://localhost:27017/controle_financeiro", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "Erro de conexão ao MongoDB:")
+);
+mongoose.connection.once("open", () => {
+  console.log("Conectado ao MongoDB");
 });
 
 // Configurar o middleware para analisar o corpo das requisições
@@ -18,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Rotas principais
-app.use('/', routes);
+app.use("/", routes);
 
 // Iniciar o servidor
 app.listen(PORT, () => {
