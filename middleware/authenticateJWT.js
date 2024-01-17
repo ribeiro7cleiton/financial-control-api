5757b1e4ed83b22542b8aca03813226a2ae097b0
Cleiton Ribeiro <ribeirocleiton@outlook.com.br>
2024-01-16 22:27:10 -0300

    adicionando o middleware authenticate

---
+++ middleware/authenticateJWT.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'seuSegredo';
+
+const authenticateJWT = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateJWT;
